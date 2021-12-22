d25d8cb40d4f2d6a9336c979f0c0524bdc298ad9
Cmunozh <cmunozh@efizity.com>
2021-12-22 11:35:49 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        Mi Primera Tienda
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Coder House
         </a>
       </header>
     </div>
